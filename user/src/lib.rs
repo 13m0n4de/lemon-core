9c6dc9e529f9167d882b58c7ce1aa921392986d8
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-25 04:25:19 +0800

    feat(syscall): add `user` lib, add `sys_write`, `sys_exit` syscall

---
+++ user/src/lib.rs
@@ -0,0 +1,42 @@
+#![no_std]
+#![feature(linkage)]
+
+mod syscall;
+
+#[no_mangle]
+#[link_section = ".text.entry"]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    exit(main());
+}
+
+#[no_mangle]
+#[linkage = "weak"]
+fn main() -> i32 {
+    panic!("Cannot find main!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn start_bss();
+        fn end_bss();
+    }
+
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            start_bss as usize as *mut u8,
+            end_bss as usize - start_bss as usize,
+        )
+        .fill(0);
+    }
+}
+
+use syscall::*;
+
+pub fn write(fd: usize, buf: &[u8]) -> isize {
+    sys_write(fd, buf)
+}
+
+pub fn exit(exit_code: i32) -> ! {
+    sys_exit(exit_code)
+}
