a439da7afed33ca226b9f28b0a10586e5391c1a0
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-25 01:22:49 +0800

    feat(console): add `console` module, implement formatted printting

---
+++ os/src/console.rs
@@ -0,0 +1,33 @@
+use crate::sbi::console_putchar;
+use core::fmt::{self, Arguments, Write};
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for b in s.bytes() {
+            console_putchar(b as usize);
+        }
+        Ok(())
+    }
+}
+
+pub fn print(args: Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+/// print string
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    };
+}
+
+/// print string with newline
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
