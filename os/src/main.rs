43b2c83f1a0467715679f8f337372fab0b425a16
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-25 01:52:19 +0800

    feat(logging): add logging support

--- os/src/main.rs
+++ os/src/main.rs
@@ -9,18 +9,54 @@
 #[macro_use]
 mod console;
 mod lang_items;
+mod logging;
 mod sbi;
 
 use core::arch::global_asm;
+use log::*;
 
 global_asm!(include_str!("entry.asm"));
 
 /// the rust entrypoint of OS
 #[no_mangle]
 pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack_lower_bound(); // stack lower bound
+        fn boot_stack_top(); // stack top
+    }
+
     clear_bss();
-    println!("Hello world!");
-    panic!("Shutdown machine!");
+    logging::init();
+
+    debug!(
+        "[kernel] .text [{:#x}, {:#x})",
+        stext as usize, etext as usize
+    );
+    debug!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    debug!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    debug!(
+        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack_lower_bound as usize
+    );
+    debug!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+
+    info!("[kernel] Hello world!");
+
+    sbi::shutdown(false)
 }
 
 /// clear BSS segment
