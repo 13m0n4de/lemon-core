e80eed14d4718db4ac0de801ebd3046041ee0184
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-25 04:40:19 +0800

    feat(console, lang_items): add panic handler and console

---
+++ user/src/console.rs
@@ -0,0 +1,33 @@
+use super::write;
+use core::fmt::{self, Arguments, Write};
+
+struct Stdout;
+
+const STDOUT: usize = 1;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        write(STDOUT, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+/// print string
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    };
+}
+
+/// print string with newline
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
