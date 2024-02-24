e579650ca1765808a61971eeff70ea2de4b093a2
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-25 01:15:53 +0800

    feat(sbi): add sbi call `console_putchar` and `shutdown`

---
+++ os/src/sbi.rs
@@ -0,0 +1,18 @@
+#![allow(unused)]
+
+/// use sbi call to putchar in console (qemu uart handler)
+pub fn console_putchar(c: usize) {
+    #[allow(deprecated)]
+    sbi_rt::legacy::console_putchar(c);
+}
+
+/// use sbi call to shutdown the kernel
+pub fn shutdown(failure: bool) -> ! {
+    use sbi_rt::{system_reset, NoReason, Shutdown, SystemFailure};
+    if !failure {
+        system_reset(Shutdown, NoReason);
+    } else {
+        system_reset(Shutdown, SystemFailure);
+    }
+    unreachable!()
+}
