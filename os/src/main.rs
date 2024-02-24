2e0404cda30ef0d487b7b34053bbe7c59b1fea15
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-25 01:22:49 +0800

    feat(console): add `console` module, implement formatted printting

--- os/src/main.rs
+++ os/src/main.rs
@@ -5,6 +5,8 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
+mod console;
 mod lang_items;
 mod sbi;
 
@@ -16,6 +18,7 @@ global_asm!(include_str!("entry.asm"));
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
+    println!("Hello world!");
     loop {}
 }
 
