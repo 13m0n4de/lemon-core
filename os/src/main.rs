76c7ab1388592071c4b07c216a11161aa54d56b0
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-25 00:51:25 +0800

    feat(entry): allocate boot stack space and clear BSS segment on startup

--- os/src/main.rs
+++ os/src/main.rs
@@ -10,3 +10,23 @@ mod lang_items;
 use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
+
+/// the rust entrypoint of OS
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    loop {}
+}
+
+/// clear BSS segment
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+
+    unsafe {
+        core::slice::from_raw_parts_mut(sbss as usize as *mut u8, ebss as usize - sbss as usize)
+            .fill(0);
+    }
+}
