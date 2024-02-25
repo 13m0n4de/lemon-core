d64695423a921f714560f7ef840531f46e0bd3c9
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-26 06:07:53 +0800

    refactor(batch, lang_items, syscall, trap): use the logging module instead of `println!` for output

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -2,19 +2,20 @@
 
 use crate::sbi::shutdown;
 use core::panic::PanicInfo;
+use log::*;
 
 #[panic_handler]
 fn panic_handler(info: &PanicInfo) -> ! {
     let message = info.message().unwrap();
     if let Some(location) = info.location() {
-        println!(
+        error!(
             "Panicked at {}:{} {}",
             location.file(),
             location.line(),
             message
         );
     } else {
-        println!("Panicked: {}", message);
+        error!("Panicked: {}", message);
     }
     shutdown(true)
 }
