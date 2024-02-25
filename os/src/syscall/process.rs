396ff1ddb475f8c5b2252be71e6ad46d73ab53ef
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-26 06:07:53 +0800

    refactor(batch, lang_items, syscall, trap): use the logging module instead of `println!` for output

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,8 +1,9 @@
 //! App management syscalls
 use crate::batch::run_next_app;
+use log::*;
 
 /// task exits and submit an exit code
 pub fn sys_exit(exit_code: i32) -> ! {
-    println!("[kernel] Application exited with code {}", exit_code);
+    info!("[kernel] Application exited with code {}", exit_code);
     run_next_app()
 }
