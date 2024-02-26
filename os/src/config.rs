fdb6b3e3d3e584f454d2566bdc24fc9ffc58bc73
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-26 15:52:40 +0800

    refactor(config): updated QEMU clock frequency from 12.5 MHz to 10.0 MHz.

--- os/src/config.rs
+++ os/src/config.rs
@@ -6,4 +6,4 @@ pub const MAX_APP_NUM: usize = 16;
 pub const APP_BASE_ADDRESS: usize = 0x80400000;
 pub const APP_SIZE_LIMIT: usize = 0x20000;
 
-pub const CLOCK_FREQ: usize = 12500000;
+pub const CLOCK_FREQ: usize = 10000000;
