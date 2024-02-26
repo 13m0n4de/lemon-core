1ed2766104bb1ba32cd94680bf95be6a98835a01
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-26 15:27:10 +0800

    feat(config, sbi, syscall, timer): add timer and syscall `sys_get_time`

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,7 +1,10 @@
 //! App management syscalls
 use log::*;
 
-use crate::task::{exit_current_and_run_next, suspend_current_and_run_next};
+use crate::{
+    task::{exit_current_and_run_next, suspend_current_and_run_next},
+    timer::get_time_ms,
+};
 
 /// task exits and submit an exit code
 pub fn sys_exit(exit_code: i32) -> ! {
@@ -14,4 +17,9 @@ pub fn sys_exit(exit_code: i32) -> ! {
 pub fn sys_yield() -> isize {
     suspend_current_and_run_next();
     0
+}
+
+/// get time in milliseconds
+pub fn sys_get_time() -> isize {
+    get_time_ms() as isize
 }
