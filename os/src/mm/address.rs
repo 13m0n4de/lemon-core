560caa9607cbd83a84aea0c9d6ab12866cc9d96d
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-28 11:16:20 +0800

    feat(config, mm): add data structure abstraction and type definitions for addresses

---
+++ os/src/mm/address.rs
@@ -0,0 +1,148 @@
+//! Implementation of physical and virtual address and page number.
+
+use crate::config::{PAGE_SIZE, PAGE_SIZE_BITS};
+
+//              Virtual Address (39 bits)
+// 38                  12 11           0
+// +---------------------+-------------+
+// | Virtual Page Number | Page Offset |
+// +---------------------+-------------+
+//
+//              Physical Address (56 bits)
+// 55                               12 11           0
+// +----------------------------------+-------------+
+// |       Physical Page Number       | Page Offset |
+// +----------------------------------+-------------+
+const PA_WIDTH_SV39: usize = 56;
+const VA_WIDTH_SV39: usize = 39;
+const PPN_WIDTH_SV39: usize = PA_WIDTH_SV39 - PAGE_SIZE_BITS;
+const VPN_WIDTH_SV39: usize = VA_WIDTH_SV39 - PAGE_SIZE_BITS;
+
+/// physical address
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct PhysAddr(pub usize);
+
+/// virtual address
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct VirtAddr(pub usize);
+
+/// physical page number
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct PhysPageNum(pub usize);
+
+/// virtual page number
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct VirtPageNum(pub usize);
+
+// From usize To PhysAddr, PhysPageNum, VirtAddr, VirtPageNum
+
+impl From<usize> for PhysAddr {
+    fn from(value: usize) -> Self {
+        Self(value & ((1 << PA_WIDTH_SV39) - 1))
+    }
+}
+
+impl From<usize> for PhysPageNum {
+    fn from(value: usize) -> Self {
+        Self(value & ((1 << PPN_WIDTH_SV39) - 1))
+    }
+}
+
+impl From<usize> for VirtAddr {
+    fn from(value: usize) -> Self {
+        Self(value & ((1 << VA_WIDTH_SV39) - 1))
+    }
+}
+
+impl From<usize> for VirtPageNum {
+    fn from(value: usize) -> Self {
+        Self(value & ((1 << VPN_WIDTH_SV39) - 1))
+    }
+}
+
+// From PhysAddr, PhysPageNum, VirtAddr, VirtPageNum To usize
+
+impl From<PhysAddr> for usize {
+    fn from(value: PhysAddr) -> Self {
+        value.0
+    }
+}
+
+impl From<PhysPageNum> for usize {
+    fn from(value: PhysPageNum) -> Self {
+        value.0
+    }
+}
+
+impl From<VirtAddr> for usize {
+    fn from(value: VirtAddr) -> Self {
+        // In SV39, the bits [63..=39] must be the same as bit 38, which means for addresses
+        // in the range 1 << 38 to 1 << 39, the bits 63..=39 are all 1s. Therefore, it's necessary
+        // to truncate all bits before bit 38.
+        if value.0 >= (1 << (VA_WIDTH_SV39 - 1)) {
+            value.0 | (!((1 << VA_WIDTH_SV39) - 1))
+        } else {
+            value.0
+        }
+    }
+}
+
+impl From<VirtPageNum> for usize {
+    fn from(value: VirtPageNum) -> Self {
+        value.0
+    }
+}
+
+// Conversion between PhysAddr and PhysPageNum
+
+impl PhysAddr {
+    /// Get the pgae offset
+    pub fn page_offset(&self) -> usize {
+        self.0 & (PAGE_SIZE - 1)
+    }
+
+    /// Checks if the address is page-aligned.
+    pub fn aligned(&self) -> bool {
+        self.page_offset() == 0
+    }
+}
+
+impl From<PhysAddr> for PhysPageNum {
+    fn from(value: PhysAddr) -> Self {
+        assert!(value.aligned());
+        PhysPageNum(value.0 / PAGE_SIZE)
+    }
+}
+
+impl From<PhysPageNum> for PhysAddr {
+    fn from(value: PhysPageNum) -> Self {
+        Self(value.0 << PAGE_SIZE_BITS)
+    }
+}
+
+// Conversion between VirtAddr and VirtPageNum
+
+impl VirtAddr {
+    /// Get the pgae offset
+    pub fn page_offset(&self) -> usize {
+        self.0 & (PAGE_SIZE - 1)
+    }
+
+    /// Checks if the address is page-aligned.
+    pub fn aligned(&self) -> bool {
+        self.page_offset() == 0
+    }
+}
+
+impl From<VirtAddr> for VirtPageNum {
+    fn from(value: VirtAddr) -> Self {
+        assert!(value.aligned());
+        VirtPageNum(value.0 / PAGE_SIZE_BITS)
+    }
+}
+
+impl From<VirtPageNum> for VirtAddr {
+    fn from(value: VirtPageNum) -> Self {
+        Self(value.0 << PAGE_SIZE_BITS)
+    }
+}
