7a505940428729069afc59c80e5e646536d5bc70
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-02-28 08:59:29 +0800

    feat(config, mm): add heap allocator and test case

--- os/src/config.rs
+++ os/src/config.rs
@@ -6,4 +6,6 @@ pub const MAX_APP_NUM: usize = 16;
 pub const APP_BASE_ADDRESS: usize = 0x80400000;
 pub const APP_SIZE_LIMIT: usize = 0x20000;
 
+pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
+
 pub const CLOCK_FREQ: usize = 10000000;
