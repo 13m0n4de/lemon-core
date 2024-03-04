aa61846560d2c23a47bc373289b308120e5e4db5
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-04 20:52:16 +0800

    fix(config, mm): fix memory lifecycle issue in PageTable
    This modification ensures that the internal memory of PageTable is not
    prematurely deallocated, resolving the issue related to the lifecycle of
    the page table's own memory.

--- os/src/config.rs
+++ os/src/config.rs
@@ -3,6 +3,7 @@
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
+pub const MEMORY_END: usize = 0x80800000;
 
 pub const PAGE_SIZE: usize = 0x1000;
 pub const PAGE_SIZE_BITS: usize = 0xc;
@@ -18,4 +19,3 @@ pub fn kernel_stack_position(app_id: usize) -> (usize, usize) {
 }
 
 pub const CLOCK_FREQ: usize = 10000000;
-pub const MEMORY_END: usize = 0x8800_0000;
