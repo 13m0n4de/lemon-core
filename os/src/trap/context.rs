04cd8be37fd2f464abdeb12feb6e6ff6c5af3c3d
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-04 12:57:48 +0800

    feat(trap): extend trap context
    save and restore trap context and switch address spaces in `__alltraps` and `__restore`

--- os/src/trap/context.rs
+++ os/src/trap/context.rs
@@ -9,6 +9,12 @@ pub struct TrapContext {
     pub sstatus: Sstatus,
     /// CSR sepc
     pub sepc: usize,
+    /// Addr of Page Table
+    pub kernel_satp: usize,
+    /// kernel stack
+    pub kernel_sp: usize,
+    /// Addr of trap_handler function
+    pub trap_handler: usize,
 }
 
 impl TrapContext {
@@ -18,13 +24,22 @@ impl TrapContext {
     }
 
     /// init app context
-    pub fn app_init_context(entry: usize, sp: usize) -> Self {
+    pub fn app_init_context(
+        entry: usize,
+        sp: usize,
+        kernel_satp: usize,
+        kernel_sp: usize,
+        trap_handler: usize,
+    ) -> Self {
         let mut sstatus = sstatus::read(); // CSR sstatus
         sstatus.set_spp(SPP::User); //previous privilege mode: user mode
         let mut cx = Self {
             x: [0; 32],
             sstatus,
-            sepc: entry, // entry point of app
+            sepc: entry,  // entry point of app
+            kernel_satp,  // addr of page table
+            kernel_sp,    // kernel stack
+            trap_handler, // addr of trap_handler function
         };
         cx.set_sp(sp); // app's user stack pointer
         cx // return initial Trap Context of app
