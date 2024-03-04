a32f605c9ab9c51ed36aa4824f361b66e2905acd
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-04 21:02:45 +0800

    refactor(trap): update trap handler

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -10,13 +10,15 @@
 mod context;
 
 use crate::{
+    config::{TRAMPOLINE, TRAP_CONTEXT},
     syscall::syscall,
     task::{
-        exit_current_and_run_next, suspend_current_and_run_next, user_time_end, user_time_start,
+        current_trap_cx, current_user_token, exit_current_and_run_next,
+        suspend_current_and_run_next, user_time_end,
     },
     timer::set_next_trigger,
 };
-use core::arch::global_asm;
+use core::arch::{asm, global_asm};
 use log::*;
 use riscv::register::{
     mtvec::TrapMode,
@@ -46,17 +48,22 @@ pub fn enable_timer_interrupt() {
 
 /// handle an interrupt, exception, or system call from user space
 #[no_mangle]
-pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
+pub fn trap_handler() -> ! {
     user_time_end();
-
+    set_kernel_trap_entry();
+    let cx = current_trap_cx();
     let scause = scause::read(); // get trap cause
     let stval = stval::read(); // get extra value
+
     match scause.cause() {
         Trap::Exception(Exception::UserEnvCall) => {
             cx.sepc += 4;
             cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
         }
-        Trap::Exception(Exception::StoreFault) | Trap::Exception(Exception::StorePageFault) => {
+        Trap::Exception(Exception::StoreFault)
+        | Trap::Exception(Exception::StorePageFault)
+        | Trap::Exception(Exception::LoadFault)
+        | Trap::Exception(Exception::LoadPageFault) => {
             info!("[kernel] PageFault in application, bad addr = {:#x}, bad instruction = {:#x}, kernel killed it.", stval, cx.sepc);
             exit_current_and_run_next();
         }
@@ -76,8 +83,7 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
             );
         }
     }
-    user_time_start();
-    cx
+    trap_return()
 }
 
 /// set the new addr of __restore asm function in TRAMPOLINE page,
@@ -85,7 +91,43 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
 /// finally, jump to new addr of __restore asm function
 #[no_mangle]
 pub fn trap_return() -> ! {
-    todo!()
+    set_user_trap_entry();
+    let trap_cx_ptr = TRAP_CONTEXT;
+    let user_satp = current_user_token();
+    extern "C" {
+        fn __alltraps();
+        fn __restore();
+    }
+    let restore_va = __restore as usize - __alltraps as usize + TRAMPOLINE;
+    unsafe {
+        asm!(
+            "fence.i",
+            "jr {restore_va}",             // jump to new addr of __restore asm function
+            restore_va = in(reg) restore_va,
+            in("a0") trap_cx_ptr,      // a0 = virt addr of Trap Context
+            in("a1") user_satp,        // a1 = phy addr of usr page table
+            options(noreturn)
+        )
+    }
+}
+
+#[no_mangle]
+/// Unimplement: traps/interrupts/exceptions from kernel mode
+/// Todo: Chapter 9: I/O device
+pub fn trap_from_kernel() -> ! {
+    panic!("a trap from kernel!");
+}
+
+fn set_kernel_trap_entry() {
+    unsafe {
+        stvec::write(trap_from_kernel as usize, TrapMode::Direct);
+    }
+}
+
+fn set_user_trap_entry() {
+    unsafe {
+        stvec::write(TRAMPOLINE as usize, TrapMode::Direct);
+    }
 }
 
 pub use context::TrapContext;
