64b462ff779075ba4e024b543f486e1304e9de7b
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-04 22:40:41 +0800

    refactor(mm, syscall): refactor `sys_write` using `translated_byte_buffer`

--- os/src/mm/page_table.rs
+++ os/src/mm/page_table.rs
@@ -1,7 +1,6 @@
+use super::{frame_alloc, FrameTracker, PhysPageNum, StepByOne, VirtAddr, VirtPageNum};
 use alloc::{collections::BTreeMap, vec, vec::Vec};
 use bitflags::*;
-
-use super::{frame_alloc, FrameTracker, PhysPageNum, VirtPageNum};
 
 bitflags! {
     /// [`PageTableEntry`] flags
@@ -144,4 +143,31 @@ impl PageTable {
     pub fn token(&self) -> usize {
         8usize << 60 | self.root_ppn.0
     }
+}
+
+/// translate a pointer to a mutable u8 Vec through page table
+pub fn translated_byte_buffer(token: usize, ptr: *const u8, len: usize) -> Vec<&'static mut [u8]> {
+    let page_table = PageTable::from_token(token);
+    let mut start = ptr as usize;
+    let end = start + len;
+    let mut v = Vec::new();
+
+    while start < end {
+        let start_va = VirtAddr::from(start);
+        let mut vpn = start_va.floor_to_vpn();
+        let ppn = page_table.translate(vpn).unwrap().ppn();
+        vpn.step();
+        let mut end_va: VirtAddr = vpn.into();
+        end_va = end_va.min(VirtAddr::from(end));
+
+        if end_va.page_offset() == 0 {
+            v.push(&mut ppn.get_bytes_array()[start_va.page_offset()..]);
+        } else {
+            v.push(&mut ppn.get_bytes_array()[start_va.page_offset()..end_va.page_offset()])
+        }
+
+        start = end_va.into();
+    }
+
+    v
 }
