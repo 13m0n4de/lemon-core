1963264c7c0c8ad4088e994e79cc0e5e3028e8c7
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-04 16:55:27 +0800

    feat(task): implement methods in `TaskManager`
    - `get_current_token`
    - `get_current_trap_cx`

---
+++ os/src/task/manager.rs
@@ -0,0 +1,114 @@
+use super::{TaskContext, TaskPool, TaskStatus, __switch};
+use crate::sbi::shutdown;
+use crate::sync::UPSafeCell;
+use crate::trap::TrapContext;
+use log::*;
+
+pub struct TaskManager {
+    pub num_app: usize,
+    pub inner: UPSafeCell<TaskPool>,
+}
+
+impl TaskManager {
+    // Run the first task in task list.
+    pub fn run_first_task(&self) -> ! {
+        let mut inner = self.inner.exclusive_access();
+        let task0 = &mut inner.tasks[0];
+
+        task0.task_status = TaskStatus::Running;
+        let next_task_cx_ptr = &task0.task_cx as *const TaskContext;
+
+        inner.refresh_stop_watch();
+
+        drop(inner);
+        let mut _unused = TaskContext::zero_init();
+        // before this, we should drop local variables that must be dropped manually
+        unsafe {
+            __switch(&mut _unused as *mut TaskContext, next_task_cx_ptr);
+        }
+        panic!("unreachable in run_first_task!");
+    }
+
+    // Find next task to run and return app id.
+    pub fn mark_current_exited(&self) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+
+        inner.tasks[current].kernel_time += inner.refresh_stop_watch();
+        trace!(
+            "[kernel] Task {} exited. user_time: {} ms, kernle_time: {} ms.",
+            current,
+            inner.tasks[current].user_time,
+            inner.tasks[current].kernel_time
+        );
+        inner.tasks[current].task_status = TaskStatus::Exited;
+    }
+
+    // Find next task to run and return app id.
+    fn find_next_task(&self) -> Option<usize> {
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        (current + 1..current + self.num_app + 1)
+            .map(|id| id % self.num_app)
+            .find(|id| inner.tasks[*id].task_status == TaskStatus::Ready)
+    }
+
+    // Change the status of current `Running` task into `Ready`.
+    pub fn mark_current_suspended(&self) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+
+        trace!("[kernel] Task {} suspended", current);
+
+        inner.tasks[current].kernel_time += inner.refresh_stop_watch();
+        inner.tasks[current].task_status = TaskStatus::Ready;
+    }
+
+    // Find the next 'Ready' task and set its status to 'Running'.
+    // Update `current_task` to this task.
+    // Call `__switch` to switch tasks.
+    pub fn run_next_task(&self) {
+        if let Some(next) = self.find_next_task() {
+            let mut inner = self.inner.exclusive_access();
+            let current = inner.current_task;
+            trace!("[kernel] Task {} start", current);
+            inner.tasks[next].task_status = TaskStatus::Running;
+            inner.current_task = next;
+            let current_task_cx_ptr = &mut inner.tasks[current].task_cx as *mut TaskContext;
+            let next_task_cx_ptr = &inner.tasks[next].task_cx as *const TaskContext;
+            drop(inner);
+            // before this, we should drop local variables that must be dropped manually
+            unsafe {
+                __switch(current_task_cx_ptr, next_task_cx_ptr);
+            }
+            // go back to user mode
+        } else {
+            info!("[kernel] All applications completed!");
+            shutdown(false);
+        }
+    }
+
+    // Counting kernel time, starting from now is user time.
+    pub fn user_time_start(&self) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].kernel_time += inner.refresh_stop_watch();
+    }
+
+    // Counting user time, starting from now is kernel time.
+    pub fn user_time_end(&self) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].user_time += inner.refresh_stop_watch();
+    }
+
+    pub fn get_current_token(&self) -> usize {
+        let inner = self.inner.exclusive_access();
+        inner.tasks[inner.current_task].get_user_token()
+    }
+
+    pub fn get_current_trap_cx(&self) -> &'static mut TrapContext {
+        let inner = self.inner.exclusive_access();
+        inner.tasks[inner.current_task].get_trap_cx()
+    }
+}
