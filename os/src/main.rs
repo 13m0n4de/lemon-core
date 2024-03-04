cc4e6d611d2eb61f2bfe0e14b467ea34f89fbe6d
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-04 22:49:22 +0800

    style(justfile, mm, task, trap, user): improve format of the code, remove unnecessary code

--- os/src/main.rs
+++ os/src/main.rs
@@ -38,7 +38,6 @@ mod timer;
 mod trap;
 
 use core::arch::global_asm;
-use log::*;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -47,17 +46,12 @@ global_asm!(include_str!("link_app.S"));
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-
     logging::init();
-    print_segment_info();
-
     mm::init();
     trap::init();
-    loader::load_apps();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
     task::run_first_task();
-
     panic!("unreachable in rust_main!");
 }
 
@@ -72,49 +66,4 @@ fn clear_bss() {
         core::slice::from_raw_parts_mut(sbss as usize as *mut u8, ebss as usize - sbss as usize)
             .fill(0);
     }
-}
-
-fn print_segment_info() {
-    /* External Symbols:
-     * - `stext`: start addr of text segment
-     * - `etext`: end addr of text segment
-     * - `srodata`: start addr of Read-Only data segment
-     * - `erodata`: end addr of Read-Only data segment
-     * - `sdata`: start addr of data segment
-     * - `edata`: end addr of data segment
-     * - `sbss`: start addr of BSS segment
-     * - `ebss`: end addr of BSS segment
-     * - `boot_stack_lower_bound`: lower bound of the boot stack
-     * - `boot_stack_top`: top addr of the boot stack
-     */
-    extern "C" {
-        fn stext();
-        fn etext();
-        fn srodata();
-        fn erodata();
-        fn sdata();
-        fn edata();
-        fn sbss();
-        fn ebss();
-        fn boot_stack_lower_bound();
-        fn boot_stack_top();
-    }
-
-    debug!(
-        "[kernel] .text [{:#x}, {:#x})",
-        stext as usize, etext as usize
-    );
-    debug!(
-        "[kernel] .rodata [{:#x}, {:#x})",
-        srodata as usize, erodata as usize
-    );
-    debug!(
-        "[kernel] .data [{:#x}, {:#x})",
-        sdata as usize, edata as usize
-    );
-    debug!(
-        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
-        boot_stack_top as usize, boot_stack_lower_bound as usize
-    );
-    debug!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
 }
