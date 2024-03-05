81b9e624839829b4dd77ed8e0412b677163a2363
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-06 01:06:06 +0800

    feat(user): add syscall `read` and function `getchar`

--- user/src/console.rs
+++ user/src/console.rs
@@ -1,8 +1,9 @@
-use super::write;
+use super::{read, write};
 use core::fmt::{self, Arguments, Write};
 
 struct Stdout;
 
+const STDIN: usize = 0;
 const STDOUT: usize = 1;
 
 impl Write for Stdout {
@@ -30,4 +31,10 @@ macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
+}
+
+pub fn getchar() -> u8 {
+    let mut c = [0u8; 1];
+    read(STDIN, &mut c);
+    c[0]
 }
