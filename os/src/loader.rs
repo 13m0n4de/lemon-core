e2ee69587c7daee8d222a86cf5792cce83e3cc5b
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-06 03:06:17 +0800

    feat(loader): add function `get_app_data_by_name` and `list_apps`

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -49,3 +49,18 @@ lazy_static! {
         v
     };
 }
+
+pub fn get_app_data_by_name(name: &str) -> Option<&'static [u8]> {
+    let num_app = get_num_app();
+    (0..num_app)
+        .find(|&i| APP_NAMES[i] == name)
+        .map(get_app_data)
+}
+
+pub fn list_apps() {
+    println!("/**** APPS ****");
+    for app in APP_NAMES.iter() {
+        println!("{}", app);
+    }
+    println!("**************/");
+}
