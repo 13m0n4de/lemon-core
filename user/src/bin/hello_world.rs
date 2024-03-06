15ff337e1f7ba7dc91d765e2f02093afda86c074
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-07 02:26:01 +0800

    test(app): add user applications

--- user/src/bin/hello_world.rs
+++ user/src/bin/hello_world.rs
@@ -4,8 +4,10 @@
 #[macro_use]
 extern crate user_lib;
 
+use user_lib::getpid;
+
 #[no_mangle]
-unsafe fn main() -> i32 {
-    println!("Hello world!");
+fn main() -> i32 {
+    println!("pid {}: Hello world from user mode program!", getpid());
     0
 }
