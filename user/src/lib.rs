5103bb24a909b32461443e6d274bcf15f7f60c6b
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-07 02:24:46 +0800

    feat(user): add sleep function for waiting a specified period of time

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -80,3 +80,10 @@ pub fn waitpid(pid: usize, exit_code: &mut i32) -> isize {
         }
     }
 }
+
+pub fn sleep(period_ms: usize) {
+    let start = sys_get_time();
+    while sys_get_time() < start + period_ms as isize {
+        sys_yield();
+    }
+}
