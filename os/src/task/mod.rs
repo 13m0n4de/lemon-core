444237827b8a9897ab47ce51a013189b657c37e1
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-07 02:37:21 +0800

    refactor(task): refactor `TaskPool` to `TaskInner` for Task structure

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -17,10 +17,10 @@ pub use context::TaskContext;
 
 struct TaskManager {
     num_app: usize,
-    inner: UPSafeCell<TaskPool>,
+    inner: UPSafeCell<TaskInner>,
 }
 
-struct TaskPool {
+struct TaskInner {
     tasks: [TaskControlBlock; MAX_APP_NUM],
     current_task: usize,
 }
@@ -39,7 +39,7 @@ enum TaskStatus {
     Exited,
 }
 
-impl TaskPool {
+impl TaskInner {
     fn new() -> Self {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
@@ -125,7 +125,7 @@ lazy_static! {
             fn _num_app();
         }
         let num_app = unsafe { (_num_app as usize as *const usize).read_volatile() };
-        let inner = unsafe { UPSafeCell::new(TaskPool::new()) };
+        let inner = unsafe { UPSafeCell::new(TaskInner::new()) };
         TaskManager { num_app, inner }
     };
 }
