e11c75ff6f5de0bfac80b2d9b0570040adee0d6b
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-06 20:05:36 +0800

    feat(task): add `TaskControlBlockInner` to represent metadata that may change during execution
    - Move `TaskControlBlock` to `control_block.rs`.
    - Change `TaskStatus::Exited` to `TaskStatus::Zombie`

---
+++ os/src/task/control_block.rs
@@ -0,0 +1,104 @@
+use alloc::sync::{Arc, Weak};
+use alloc::vec::Vec;
+use core::cell::RefMut;
+
+use crate::config::TRAP_CONTEXT;
+use crate::mm::{MemorySet, PhysPageNum, VirtAddr, KERNEL_SPACE};
+use crate::sync::UPSafeCell;
+use crate::trap::{trap_handler, TrapContext};
+
+use super::context::TaskContext;
+use super::pid::{pid_alloc, KernelStack, PidHandle};
+use super::TaskStatus;
+
+pub struct TaskControlBlock {
+    pub pid: PidHandle,
+    pub kernel_stack: KernelStack,
+    inner: UPSafeCell<TaskControlBlockInner>,
+}
+
+impl TaskControlBlock {
+    pub fn new(elf_data: &[u8]) -> Self {
+        // memory_set with elf program headers/trampoline/trap context/user stack
+        let (memory_set, user_sp, entry_point) = MemorySet::from_elf(elf_data);
+        let trap_cx_ppn = memory_set
+            .translate(VirtAddr::from(TRAP_CONTEXT).into())
+            .unwrap()
+            .ppn();
+        let task_status = TaskStatus::Ready;
+
+        // alloc a pid and a kernel stack in kernel space
+        let pid_handle = pid_alloc();
+        let kernel_stack = KernelStack::new(&pid_handle);
+        let kernel_stack_top = kernel_stack.get_top();
+
+        // push a task context with goes to trap_return to the top of kernel stack
+        let task_control_block = Self {
+            pid: pid_handle,
+            kernel_stack,
+            inner: unsafe {
+                UPSafeCell::new(TaskControlBlockInner {
+                    trap_cx_ppn,
+
+                    base_size: user_sp,
+
+                    task_status,
+                    task_cx: TaskContext::goto_trap_return(kernel_stack_top),
+                    memory_set,
+
+                    parent: None,
+                    children: Vec::new(),
+                    exit_code: 0,
+                })
+            },
+        };
+
+        // prepare TrapContext in user space
+        let trap_cx = task_control_block.inner_exclusive_access().get_trap_cx();
+        *trap_cx = TrapContext::app_init_context(
+            entry_point,
+            user_sp,
+            KERNEL_SPACE.exclusive_access().token(),
+            kernel_stack_top,
+            trap_handler as usize,
+        );
+
+        task_control_block
+    }
+
+    pub fn inner_exclusive_access(&self) -> RefMut<'_, TaskControlBlockInner> {
+        self.inner.exclusive_access()
+    }
+}
+
+struct TaskControlBlockInner {
+    trap_cx_ppn: PhysPageNum,
+
+    base_size: usize,
+
+    task_status: TaskStatus,
+    task_cx: TaskContext,
+    memory_set: MemorySet,
+
+    parent: Option<Weak<TaskControlBlock>>,
+    children: Vec<Arc<TaskControlBlock>>,
+    exit_code: i32,
+}
+
+impl TaskControlBlockInner {
+    pub fn get_trap_cx(&self) -> &'static mut TrapContext {
+        self.trap_cx_ppn.get_mut()
+    }
+
+    pub fn get_user_token(&self) -> usize {
+        self.memory_set.token()
+    }
+
+    fn get_status(&self) -> TaskStatus {
+        self.task_status
+    }
+
+    pub fn is_zombie(&self) -> bool {
+        self.get_status() == TaskStatus::Zombie
+    }
+}
