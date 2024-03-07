b38dcc1b6ee43e4c50c7da84703e896cfc49276c
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-08 00:53:24 +0800

    feat(easy-fs): add `BlockCache` struct for caching block data and operations

---
+++ easy-fs/src/block_cache.rs
@@ -0,0 +1,76 @@
+use alloc::sync::Arc;
+
+use crate::{block_dev::BlockDevice, BLOCK_SIZE};
+
+/// Cached block inside memory
+pub struct BlockCache {
+    /// cached block data
+    cache: [u8; BLOCK_SIZE],
+    /// underlying block id
+    block_id: usize,
+    /// underlying block device
+    block_device: Arc<dyn BlockDevice>,
+    /// whether the block is dirty
+    modified: bool,
+}
+
+impl BlockCache {
+    /// Load a new [`BlockCache`] from disk
+    pub fn new(block_id: usize, block_device: Arc<dyn BlockDevice>) -> Self {
+        let mut cache = [0u8; BLOCK_SIZE];
+        block_device.read_block(block_id, &mut cache);
+        Self {
+            cache,
+            block_id,
+            block_device,
+            modified: false,
+        }
+    }
+
+    /// Get the address of an offset inside the cached block data
+    fn addr_of_offset(&self, offset: usize) -> usize {
+        &self.cache[offset] as *const _ as usize
+    }
+
+    pub fn as_ref<T>(&self, offset: usize) -> &T
+    where
+        T: Sized,
+    {
+        let type_size = core::mem::size_of::<T>();
+        assert!(offset + type_size <= BLOCK_SIZE);
+        let addr = self.addr_of_offset(offset);
+        unsafe { &*(addr as *const T) }
+    }
+
+    pub fn as_mut_ref<T>(&mut self, offset: usize) -> &mut T
+    where
+        T: Sized,
+    {
+        let type_size = core::mem::size_of::<T>();
+        assert!(offset + type_size <= BLOCK_SIZE);
+        self.modified = true;
+        let addr = self.addr_of_offset(offset);
+        unsafe { &mut *(addr as *mut T) }
+    }
+
+    pub fn sync(&mut self) {
+        if self.modified {
+            self.modified = false;
+            self.block_device.write_block(self.block_id, &self.cache);
+        }
+    }
+
+    pub fn read<T, V>(&self, offset: usize, f: impl FnOnce(&T) -> V) -> V {
+        f(self.as_ref(offset))
+    }
+
+    pub fn modify<T, V>(&mut self, offset: usize, f: impl FnOnce(&mut T) -> V) -> V {
+        f(self.as_mut_ref(offset))
+    }
+}
+
+impl Drop for BlockCache {
+    fn drop(&mut self) {
+        self.sync()
+    }
+}
