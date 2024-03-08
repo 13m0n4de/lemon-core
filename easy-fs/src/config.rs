6ff1e401f9f8504f0267e2edfd84863b9c90f2af
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-08 18:36:30 +0800

    feat(easy-fs): add `DiskInode` struct

--- easy-fs/src/config.rs
+++ easy-fs/src/config.rs
@@ -2,9 +2,10 @@
 pub const BLOCK_SIZE: usize = 512;
 /// Number of bits in a block
 pub const BLOCK_BITS: usize = BLOCK_SIZE * 8;
-
 /// Use a block cache of 16 blocks
 pub const BLOCK_CACHE_SIZE: usize = 16;
 
 /// Magic number for sanity check
 pub const EFS_MAGIC: u32 = 0x3b800001;
+/// The max number of direct inodes
+pub const INODE_DIRECT_COUNT: usize = 28;
