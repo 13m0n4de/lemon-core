476441a253efd9f128600c44b8bda74c627c5dae
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-09 00:32:00 +0800

    feat(easy-fs): add method to find inode by name in VFS

--- easy-fs/src/vfs.rs
+++ easy-fs/src/vfs.rs
@@ -2,7 +2,11 @@ use alloc::sync::Arc;
 use spin::Mutex;
 
 use crate::{
-    block_cache::get_block_cache, block_dev::BlockDevice, efs::EasyFileSystem, layout::DiskInode,
+    block_cache::get_block_cache,
+    block_dev::BlockDevice,
+    config::DIRENT_SIZE,
+    efs::EasyFileSystem,
+    layout::{DirEntry, DiskInode},
 };
 
 /// Virtual filesystem layer over easy-fs
@@ -40,5 +44,39 @@ impl Inode {
         get_block_cache(self.block_id, Arc::clone(&self.block_device))
             .lock()
             .modify(self.block_offset, f)
+    }
+
+    /// Find inode under a disk inode by name
+    fn find_inode_id(&self, name: &str, disk_inode: &DiskInode) -> Option<u32> {
+        // assert it is a directory
+        assert!(disk_inode.is_dir());
+        let file_count = (disk_inode.size as usize) / DIRENT_SIZE;
+        let mut dirent = DirEntry::empty();
+        for i in 0..file_count {
+            assert_eq!(
+                disk_inode.read_at(DIRENT_SIZE * i, dirent.as_mut_bytes(), &self.block_device,),
+                DIRENT_SIZE,
+            );
+            if dirent.name() == name {
+                return Some(dirent.inode_number() as u32);
+            }
+        }
+        None
+    }
+
+    /// Find inode under current inode by name
+    pub fn find(&self, name: &str) -> Option<Arc<Inode>> {
+        let fs = self.fs.lock();
+        self.read_disk_inode(|disk_inode| {
+            self.find_inode_id(name, disk_inode).map(|inode_id| {
+                let (block_id, block_offset) = fs.disk_inode_position(inode_id);
+                Arc::new(Self::new(
+                    block_id,
+                    block_offset,
+                    self.fs.clone(),
+                    self.block_device.clone(),
+                ))
+            })
+        })
     }
 }
