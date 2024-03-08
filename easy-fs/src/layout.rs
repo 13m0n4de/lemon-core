254374ffd28a1ad18efc1afdbe9fdb37710f6c03
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-08 19:02:34 +0800

    feat(easy-fs): add `get_block_id` method to `DiskInode` for block retrieval

--- easy-fs/src/layout.rs
+++ easy-fs/src/layout.rs
@@ -1,4 +1,13 @@
-use crate::config::{EFS_MAGIC, INODE_DIRECT_COUNT};
+use alloc::sync::Arc;
+
+use crate::{
+    block_cache::get_block_cache,
+    block_dev::BlockDevice,
+    config::{BLOCK_SIZE, EFS_MAGIC, INDIRECT1_BOUND, INODE_DIRECT_COUNT, INODE_INDIRECT1_COUNT},
+};
+
+/// A indirect block
+type IndirectBlock = [u32; BLOCK_SIZE / 4];
 
 /// Super block of a filesystem
 #[repr(C)]
@@ -68,8 +77,34 @@ impl DiskInode {
     pub fn is_dir(&self) -> bool {
         self._type == DiskInodeType::Directory
     }
+
     /// Whether this inode is a file
     pub fn is_file(&self) -> bool {
         self._type == DiskInodeType::File
+    }
+
+    pub fn get_block_id(&self, inner_id: u32, block_device: &Arc<dyn BlockDevice>) -> u32 {
+        let inner_id = inner_id as usize;
+        if inner_id < INODE_DIRECT_COUNT {
+            self.direct[inner_id]
+        } else if inner_id < INDIRECT1_BOUND {
+            get_block_cache(self.indirect1 as usize, Arc::clone(block_device))
+                .lock()
+                .read(0, |indirect_block: &IndirectBlock| {
+                    indirect_block[inner_id - INODE_DIRECT_COUNT]
+                })
+        } else {
+            let last = inner_id - INDIRECT1_BOUND;
+            let indirect1 = get_block_cache(self.indirect2 as usize, Arc::clone(block_device))
+                .lock()
+                .read(0, |indirect2: &IndirectBlock| {
+                    indirect2[last / INODE_INDIRECT1_COUNT]
+                });
+            get_block_cache(indirect1 as usize, Arc::clone(block_device))
+                .lock()
+                .read(0, |indirect1: &IndirectBlock| {
+                    indirect1[last % INODE_INDIRECT1_COUNT]
+                })
+        }
     }
 }
