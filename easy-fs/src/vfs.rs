6ea9ddeec3b32bac3d803525069917a18b1fd693
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-09 00:20:54 +0800

    feat(easy-fs): add `Inode` struct for virtual filesystem layer

---
+++ easy-fs/src/vfs.rs
@@ -0,0 +1,44 @@
+use alloc::sync::Arc;
+use spin::Mutex;
+
+use crate::{
+    block_cache::get_block_cache, block_dev::BlockDevice, efs::EasyFileSystem, layout::DiskInode,
+};
+
+/// Virtual filesystem layer over easy-fs
+pub struct Inode {
+    block_id: usize,
+    block_offset: usize,
+    fs: Arc<Mutex<EasyFileSystem>>,
+    block_device: Arc<dyn BlockDevice>,
+}
+
+impl Inode {
+    pub fn new(
+        block_id: u32,
+        block_offset: usize,
+        fs: Arc<Mutex<EasyFileSystem>>,
+        block_device: Arc<dyn BlockDevice>,
+    ) -> Self {
+        Self {
+            block_id: block_id as usize,
+            block_offset,
+            fs,
+            block_device,
+        }
+    }
+
+    /// Call a function over a disk inode to read it
+    fn read_disk_inode<V>(&self, f: impl FnOnce(&DiskInode) -> V) -> V {
+        get_block_cache(self.block_id, Arc::clone(&self.block_device))
+            .lock()
+            .read(self.block_offset, f)
+    }
+
+    /// Call a function over a disk inode to modify it
+    fn modify_disk_inode<V>(&self, f: impl FnOnce(&mut DiskInode) -> V) -> V {
+        get_block_cache(self.block_id, Arc::clone(&self.block_device))
+            .lock()
+            .modify(self.block_offset, f)
+    }
+}
