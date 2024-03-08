d6ef18476903c28b11fe016ed1c8ff9b85af1524
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-08 18:36:30 +0800

    feat(easy-fs): add `DiskInode` struct

--- easy-fs/src/layout.rs
+++ easy-fs/src/layout.rs
@@ -1,4 +1,4 @@
-use crate::config::EFS_MAGIC;
+use crate::config::{EFS_MAGIC, INODE_DIRECT_COUNT};
 
 /// Super block of a filesystem
 #[repr(C)]
@@ -34,5 +34,42 @@ impl SuperBlock {
     /// Check if a super block is valid using efs magic
     pub fn is_valid(&self) -> bool {
         self.magic == EFS_MAGIC
+    }
+}
+
+/// Type of a disk inode
+#[derive(PartialEq)]
+pub enum DiskInodeType {
+    File,
+    Directory,
+}
+
+/// A disk inode
+#[repr(C)]
+pub struct DiskInode {
+    pub size: u32,
+    _type: DiskInodeType,
+    pub direct: [u32; INODE_DIRECT_COUNT],
+    pub indirect1: u32,
+    pub indirect2: u32,
+}
+
+impl DiskInode {
+    /// Initialize a disk inode
+    pub fn initialize(&mut self, inode_type: DiskInodeType) {
+        self.size = 0;
+        self._type = inode_type;
+        self.direct.fill(0);
+        self.indirect1 = 0;
+        self.indirect2 = 0;
+    }
+
+    /// Whether this inode is a directory
+    pub fn is_dir(&self) -> bool {
+        self._type == DiskInodeType::Directory
+    }
+    /// Whether this inode is a file
+    pub fn is_file(&self) -> bool {
+        self._type == DiskInodeType::File
     }
 }
