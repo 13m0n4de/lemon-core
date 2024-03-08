374ef9fd47e3e297e5007f92663f947b0da316de
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-08 17:09:48 +0800

    feat(easy-fs): add `dealloc` method for `Bitmap`

--- easy-fs/src/bitmap.rs
+++ easy-fs/src/bitmap.rs
@@ -1,12 +1,9 @@
 use alloc::sync::Arc;
 
-use crate::{block_cache::get_block_cache, block_dev::BlockDevice, config::BLOCK_SIZE};
+use crate::{block_cache::get_block_cache, block_dev::BlockDevice, config::BLOCK_BITS};
 
 /// A bitmap block
 type BitmapBlock = [u64; 64];
-
-/// Number of bits in a block
-const BLOCK_BITS: usize = BLOCK_SIZE * 8;
 
 pub struct Bitmap {
     start_block_id: usize,
@@ -24,26 +21,46 @@ impl Bitmap {
     /// Allocate a new block from a block device
     pub fn alloc(&self, block_device: &Arc<dyn BlockDevice>) -> Option<usize> {
         for block_id in 0..self.blocks {
-            let pos = get_block_cache(self.start_block_id + block_id, Arc::clone(block_device))
+            let id = get_block_cache(self.start_block_id + block_id, Arc::clone(block_device))
                 .lock()
                 .modify(0, |bitmap_block: &mut BitmapBlock| {
                     match bitmap_block
                         .iter()
                         .enumerate()
                         .find(|(_, &bit64)| bit64 != u64::MAX)
-                        .map(|(bits64_pos, bits64)| (bits64_pos, bits64.trailing_ones() as usize))
+                        .map(|(bits64_id, bits64)| (bits64_id, bits64.trailing_ones() as usize))
                     {
-                        Some((bit64_pos, inner_pos)) => {
-                            bitmap_block[bit64_pos] |= 1u64 << inner_pos;
-                            Some(block_id * BLOCK_BITS + bit64_pos * 64 + inner_pos as usize)
+                        Some((bit64_id, inner_id)) => {
+                            bitmap_block[bit64_id] |= 1u64 << inner_id;
+                            Some(block_id * BLOCK_BITS + bit64_id * 64 + inner_id as usize)
                         }
                         None => None,
                     }
                 });
-            if pos.is_some() {
-                return pos;
+            if id.is_some() {
+                return id;
             }
         }
         None
     }
+
+    /// Deallocate a block
+    pub fn dealloc(&self, block_device: &Arc<dyn BlockDevice>, bit: usize) {
+        let (block_id, bits64_id, inner_id) = decomposition(bit);
+        get_block_cache(self.start_block_id + block_id, Arc::clone(block_device))
+            .lock()
+            .modify(0, |bitmap_block: &mut BitmapBlock| {
+                assert!(bitmap_block[bits64_id] & (1u64 << inner_id) > 0);
+                bitmap_block[bits64_id] &= !(1u64 << inner_id);
+            });
+    }
+}
+
+/// Decompose bits into (block_id, bits64_id, inner_id)
+fn decomposition(bit: usize) -> (usize, usize, usize) {
+    let block_id = bit / BLOCK_BITS;
+    let bit_in_block = bit % BLOCK_BITS;
+    let bit64_id = bit_in_block / 64;
+    let inner_id = bit_in_block % 64;
+    (block_id, bit64_id, inner_id)
 }
