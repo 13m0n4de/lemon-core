2c1ebeaefc824999d2c7eddb59be439f0e535471
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-08 19:02:34 +0800

    feat(easy-fs): add `get_block_id` method to `DiskInode` for block retrieval

--- easy-fs/src/config.rs
+++ easy-fs/src/config.rs
@@ -7,5 +7,13 @@ pub const BLOCK_CACHE_SIZE: usize = 16;
 
 /// Magic number for sanity check
 pub const EFS_MAGIC: u32 = 0x3b800001;
+
 /// The max number of direct inodes
 pub const INODE_DIRECT_COUNT: usize = 28;
+/// The upper bound of direct inode index
+pub const DIRECT_BOUND: usize = INODE_DIRECT_COUNT;
+
+/// The max number of indirect1 inodes
+pub const INODE_INDIRECT1_COUNT: usize = BLOCK_SIZE / 4;
+/// The upper bound of indirect1 inode index
+pub const INDIRECT1_BOUND: usize = DIRECT_BOUND + INODE_INDIRECT1_COUNT;
