128d222c022ba9af332fc51e37292a615d5c9c95
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-09 01:42:36 +0800

    feat(easy-fs): add `create` method to create an inode in VFS

--- easy-fs/src/vfs.rs
+++ easy-fs/src/vfs.rs
@@ -1,12 +1,12 @@
 use alloc::{string::String, sync::Arc, vec::Vec};
-use spin::Mutex;
+use spin::{Mutex, MutexGuard};
 
 use crate::{
-    block_cache::get_block_cache,
+    block_cache::{block_cache_sync_all, get_block_cache},
     block_dev::BlockDevice,
     config::DIRENT_SIZE,
     efs::EasyFileSystem,
-    layout::{DirEntry, DiskInode},
+    layout::{DirEntry, DiskInode, DiskInodeType},
 };
 
 /// Virtual filesystem layer over easy-fs
@@ -46,6 +46,24 @@ impl Inode {
             .modify(self.block_offset, f)
     }
 
+    // Increase the size of a disk inode
+    fn increase_size(
+        &self,
+        new_size: u32,
+        disk_inode: &mut DiskInode,
+        fs: &mut MutexGuard<EasyFileSystem>,
+    ) {
+        if new_size < disk_inode.size {
+            return;
+        }
+        let blocks_needed = disk_inode.blocks_num_needed(new_size);
+        let mut v: Vec<u32> = Vec::new();
+        for _ in 0..blocks_needed {
+            v.push(fs.alloc_data());
+        }
+        disk_inode.increase_size(new_size, v, &self.block_device);
+    }
+
     /// Find inode under a disk inode by name
     fn find_inode_id(&self, name: &str, disk_inode: &DiskInode) -> Option<u32> {
         // assert it is a directory
@@ -58,7 +76,7 @@ impl Inode {
                 DIRENT_SIZE,
             );
             if dirent.name() == name {
-                return Some(dirent.inode_number() as u32);
+                return Some(dirent.inode_number());
             }
         }
         None
@@ -96,5 +114,58 @@ impl Inode {
             }
             v
         })
+    }
+
+    /// Create inode under current inode by name
+    pub fn create(&self, name: &str) -> Option<Arc<Inode>> {
+        let mut fs = self.fs.lock();
+
+        let op = |root_inode: &DiskInode| {
+            // assert it is a directory
+            assert!(root_inode.is_dir());
+            // has the file been created?
+            self.find_inode_id(name, root_inode)
+        };
+        if self.read_disk_inode(op).is_some() {
+            return None;
+        }
+
+        // create a new file
+        // alloc a inode with an indirect block
+        let new_inode_id = fs.alloc_inode();
+        // initialize inode
+        let (new_inode_block_id, new_inode_block_offset) = fs.disk_inode_position(new_inode_id);
+        get_block_cache(new_inode_block_id as usize, Arc::clone(&self.block_device))
+            .lock()
+            .modify(new_inode_block_offset, |new_inode: &mut DiskInode| {
+                new_inode.initialize(DiskInodeType::File);
+            });
+
+        self.modify_disk_inode(|root_inode| {
+            // append file in the dirent
+            let file_count = (root_inode.size as usize) / DIRENT_SIZE;
+            let new_size = (file_count + 1) * DIRENT_SIZE;
+            // increase size
+            self.increase_size(new_size as u32, root_inode, &mut fs);
+            // write dirent
+            let dirent = DirEntry::new(name, new_inode_id);
+            root_inode.write_at(
+                file_count * DIRENT_SIZE,
+                dirent.as_bytes(),
+                &self.block_device,
+            );
+        });
+
+        let (block_id, block_offset) = fs.disk_inode_position(new_inode_id);
+        block_cache_sync_all();
+
+        // return inode
+        Some(Arc::new(Self::new(
+            block_id,
+            block_offset,
+            self.fs.clone(),
+            self.block_device.clone(),
+        )))
+        // release efs lock automatically by compiler
     }
 }
