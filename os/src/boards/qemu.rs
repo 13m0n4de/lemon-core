38a6562fefab87363cbbc3d5ea65c9757f547067
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-09 21:08:24 +0800

    test(boards, drivers): add test functions; update block module visibility

--- os/src/boards/qemu.rs
+++ os/src/boards/qemu.rs
@@ -7,4 +7,4 @@ pub const MMIO: &[(usize, usize)] = &[
     (0x1000_1000, 0x00_1000), // Virtio Block in virt machine
 ];
 
-pub type BlockDeviceImpl = crate::drivers::VirtIOBlock;
+pub type BlockDeviceImpl = crate::drivers::block::VirtIOBlock;
