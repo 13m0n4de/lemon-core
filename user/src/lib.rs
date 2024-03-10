22326290cfa234fbd484f8851f90f802d517ccc4
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-10 21:42:21 +0800

    feat(user): get cmdargs from user stack

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -3,26 +3,43 @@
 #![feature(panic_info_message)]
 #![feature(alloc_error_handler)]
 
+extern crate alloc;
+
 #[macro_use]
 pub mod console;
 mod heap_allocator;
 mod lang_items;
 mod syscall;
 
+use alloc::vec::Vec;
 use bitflags::bitflags;
 use heap_allocator::init_heap;
 use syscall::*;
 
 #[no_mangle]
 #[link_section = ".text.entry"]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(argc: usize, argv: usize) -> ! {
     init_heap();
-    exit(main())
+    let args: Vec<&'static str> = (0..argc)
+        .map(|i| {
+            let str_start = unsafe {
+                ((argv + i * core::mem::size_of::<usize>()) as *const usize).read_volatile()
+            };
+            let len = (0usize..)
+                .find(|i| unsafe { ((str_start + *i) as *const u8).read_volatile() == 0 })
+                .unwrap();
+            core::str::from_utf8(unsafe {
+                core::slice::from_raw_parts(str_start as *const u8, len)
+            })
+            .unwrap()
+        })
+        .collect();
+    exit(main(argc, args.as_slice()))
 }
 
 #[no_mangle]
 #[linkage = "weak"]
-fn main() -> i32 {
+fn main(_argc: usize, _argv: &[&str]) -> i32 {
     panic!("Cannot find main!");
 }
 
@@ -76,8 +93,8 @@ pub fn fork() -> isize {
     sys_fork()
 }
 
-pub fn exec(path: &str) -> isize {
-    sys_exec(path)
+pub fn exec(path: &str, args: &[*const u8]) -> isize {
+    sys_exec(path, args)
 }
 
 pub fn wait(exit_code: &mut i32) -> isize {
