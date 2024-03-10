e942dc2a44421e4ff9629ae427c2be9effe7ea03
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-10 22:57:02 +0800

    feat(user): add user application `cat`

---
+++ user/src/bin/cat.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate user_lib;
+extern crate alloc;
+
+use user_lib::{close, open, read, OpenFlags};
+
+#[no_mangle]
+pub fn main(_argc: usize, argv: &[&str]) -> i32 {
+    for filename in &argv[1..] {
+        let fd = open(argv[1], OpenFlags::RDONLY);
+        if fd == -1 {
+            println!("{}: No such file or directory", filename);
+            continue;
+        }
+
+        let mut buf = [0u8; 256];
+        let fd = fd as usize;
+
+        loop {
+            let size = read(fd, &mut buf) as usize;
+            if size == 0 {
+                break;
+            }
+            print!("{}", core::str::from_utf8(&buf[..size]).unwrap());
+        }
+        print!("\n");
+
+        close(fd);
+    }
+    0
+}
