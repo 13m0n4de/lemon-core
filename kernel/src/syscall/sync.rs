e39ed4d2b8b08353aaf34c5df6b833d586b5cedb
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-19 10:18:16 +0800

    refactor(sync): refactor syscall `mutex_xxxx` and `semaphore_xxxx`

--- kernel/src/syscall/sync.rs
+++ kernel/src/syscall/sync.rs
@@ -41,30 +41,32 @@ pub fn sys_mutex_lock(mutex_id: usize) -> isize {
     let process = current_process();
     let process_inner = process.inner_exclusive_access();
 
-    if let Some(mutex) = process_inner.mutex_list.get(mutex_id) {
-        if let Some(mutex_ref) = mutex.clone().as_ref() {
+    match process_inner.mutex_list.get(mutex_id) {
+        Some(Some(mutex)) => {
+            let mutex = Arc::clone(mutex);
             drop(process_inner);
             drop(process);
-            mutex_ref.lock();
-            return 0;
+            mutex.lock();
+            0
         }
+        _ => -1,
     }
-    -1
 }
 
 pub fn sys_mutex_unlock(mutex_id: usize) -> isize {
     let process = current_process();
     let process_inner = process.inner_exclusive_access();
 
-    if let Some(mutex) = process_inner.mutex_list.get(mutex_id) {
-        if let Some(mutex_ref) = mutex.clone().as_ref() {
+    match process_inner.mutex_list.get(mutex_id) {
+        Some(Some(mutex)) => {
+            let mutex = Arc::clone(mutex);
             drop(process_inner);
             drop(process);
-            mutex_ref.unlock();
-            return 0;
+            mutex.unlock();
+            0
         }
+        _ => -1,
     }
-    -1
 }
 
 pub fn sys_semaphore_create(res_count: usize) -> isize {
@@ -90,30 +92,32 @@ pub fn sys_semaphore_up(sem_id: usize) -> isize {
     let process = current_process();
     let process_inner = process.inner_exclusive_access();
 
-    if let Some(semaphore) = process_inner.semaphore_list.get(sem_id) {
-        if let Some(semaphore_ref) = semaphore.clone().as_ref() {
+    match process_inner.semaphore_list.get(sem_id) {
+        Some(Some(semaphore)) => {
+            let semaphore = Arc::clone(semaphore);
             drop(process_inner);
             drop(process);
-            semaphore_ref.up();
-            return 0;
+            semaphore.up();
+            0
         }
+        _ => -1,
     }
-    -1
 }
 
 pub fn sys_semaphore_down(sem_id: usize) -> isize {
     let process = current_process();
     let process_inner = process.inner_exclusive_access();
 
-    if let Some(semaphore) = process_inner.semaphore_list.get(sem_id) {
-        if let Some(semaphore_ref) = semaphore.clone().as_ref() {
+    match process_inner.semaphore_list.get(sem_id) {
+        Some(Some(semaphore)) => {
+            let semaphore = Arc::clone(semaphore);
             drop(process_inner);
             drop(process);
-            semaphore_ref.down();
-            return 0;
+            semaphore.down();
+            0
         }
+        _ => -1,
     }
-    -1
 }
 
 pub fn sys_condvar_create() -> isize {
