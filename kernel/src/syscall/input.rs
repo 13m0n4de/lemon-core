a2dc9d6216832365b2d0d0b24489aac02ab462e7
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-03-27 03:05:51 +0800

    feat(syscall): add syscall `event_get` and `key_pressed`

---
+++ kernel/src/syscall/input.rs
@@ -0,0 +1,22 @@
+use crate::drivers::{KEYBOARD_DEVICE, MOUSE_DEVICE, UART};
+
+pub fn sys_event_get() -> isize {
+    let keyboard = KEYBOARD_DEVICE.clone();
+    let mouse = MOUSE_DEVICE.clone();
+
+    if !keyboard.is_empty() {
+        keyboard.read_event() as isize
+    } else if !mouse.is_empty() {
+        mouse.read_event() as isize
+    } else {
+        0
+    }
+}
+
+pub fn sys_key_pressed() -> isize {
+    if UART.is_read_buffer_empty() {
+        0
+    } else {
+        1
+    }
+}
