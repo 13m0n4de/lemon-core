d0c1b830dded2f39d63fb08b8a46acb78e4f2482
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-04-01 12:36:53 +0800

    style(user): resolve clippy lints #3

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![feature(linkage)]
 #![feature(panic_info_message)]
+#![deny(clippy::all)]
+#![deny(clippy::pedantic)]
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::missing_panics_doc)]
+#![allow(clippy::must_use_candidate)]
 
 #[macro_use]
 pub mod console;
@@ -15,11 +20,11 @@ pub extern "C" fn _start() -> ! {
 
 #[no_mangle]
 #[linkage = "weak"]
-fn main() -> i32 {
+pub extern "Rust" fn main() -> i32 {
     panic!("Cannot find main!");
 }
 
-use syscall::*;
+use syscall::{sys_exit, sys_get_time, sys_write, sys_yield};
 
 pub fn write(fd: usize, buf: &[u8]) -> isize {
     sys_write(fd, buf)
